644580d42781cd8c066c7c4a4b0e42b00c69e7b3
williamb-egov <william.burress@egov.com>
2015-12-14 08:54:17 -0500

    refactored to use SLDS090 🐷

--- src/aura/svg/svgRenderer.js
+++ src/aura/svg/svgRenderer.js
@@ -1,14 +1,15 @@
 ({
-	//Overwrite default render function for svg component
-	render: function(component, helper){
-		var classname = component.get("v.class");
-		var xlinkhref = component.get("v.xlinkHref");
-		var ariaHidden = component.get("v.ariaHidden");
+  render: function(component, helper) {
+    //grab attributes from the component markup
+    var classname = component.get("v.class");
+    var xlinkhref = component.get("v.xlinkHref");
+    var ariaHidden = component.get("v.ariaHidden");
 
-		var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-		svg.setAttribute('class', classname);
-		svg.setAttribute('aria-hidden', ariaHidden);
-		svg.innerHTML = '<use xlink:href="'+ xlinkhref +'"></use>';
-		return svg;
-	}
+    //return an svg element w/ the attributes
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute('class', classname);
+    svg.setAttribute('aria-hidden', ariaHidden);
+    svg.innerHTML = '<use xlink:href="'+xlinkhref+'"></use>';
+    return svg;
+  }
 })
