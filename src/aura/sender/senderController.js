55e49e98954e61ea4e58467ef5ba30dcc235be47
williamb-egov <william.burress@egov.com>
2015-12-14 11:16:20 -0500

    working through the event examples with sender / receiver.

---
+++ src/aura/sender/senderController.js
@@ -0,0 +1,8 @@
+({
+  send: function(component, event, helper) {
+      var text = event.source.get("v.label");
+      $A.get("e.c:message").setParams({
+          text: text
+      }).fire();
+  }//send
+})
